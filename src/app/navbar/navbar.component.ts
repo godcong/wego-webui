9b6358c598e4fd2db7dc477adc66b5968cf5f219
godcong <jumbycc@163.com>
2018-12-02 03:34:26 +0800

    change css to scss

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import {UserService} from '../user.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'],
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
 
