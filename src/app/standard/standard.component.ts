55d5747a29bb0ab2c2ea0765dcbd1219e3304ac6
godcong <jumbycc@163.com>
2018-12-14 23:21:54 +0800

    add standard database

--- src/app/standard/standard.component.ts
+++ src/app/standard/standard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-standard',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StandardComponent implements OnInit {
 
-  constructor() { }
+  databases = ['Mysql', 'Sqlite', 'Postgre'];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
