6580ee1c8fdd3c1910bfaf1c07b36bb95804bb27
godcong <jumbycc@163.com>
2018-12-20 01:02:54 +0800

    fix app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AnalyticsService} from './@core/utils/analytics.service';
+
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'WechatService';
+export class AppComponent implements OnInit {
+  constructor(private analytics: AnalyticsService) {
+  }
+
+  ngOnInit(): void {
+    this.analytics.trackPageViews();
+  }
 }
