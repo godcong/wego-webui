4f21d5ed3564d5f5d25399d75e22a685d08e262a
godcong <jumbycc@163.com>
2019-04-04 04:03:41 +0800

    update webui

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,19 +1,13 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {PagesComponent} from './pages.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: PagesComponent,
-    children: [{
-      path: 'dashboard',
-      component: DashboardComponent,
-    },],
-  },
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  }
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,12 +2,11 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {PagesComponent} from './pages.component';
 import {PagesRoutingModule} from './pages-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 @NgModule({
-  declarations: [PagesComponent, DashboardComponent],
+  declarations: [PagesComponent],
 
   imports: [
     BrowserModule,
